3a466b3eaee34d86dff24123ef84e04b71461503
John Luo <u7109933@anu.edu.au>
2024-06-19 20:08:56 +1000

    fix dorado

--- utils/base_calling.py
+++ utils/base_calling.py
@@ -6,22 +6,16 @@ import time
 from utils.configs import *
 from utils.utils import *
 
-def base_calling_guppy(prefix: str, idir: str, odir: str, barcodes: list, run_through=True):
+def base_calling_guppy(prefix: str, idir: str, odir: str, tmpdir: str, barcodes: list):
     print(f">>>Guppy Base calling..")
     ts = time.time()
 
     skip_basecalling = False
     if all(os.path.isdir(f"{odir}/{barcode}") for barcode in barcodes):
-        if run_through:
-            print(
-                "Found all intermediate directories required from configuration, skip base calling.."
-            )
-            skip_basecalling = True
-        else:
-            print(
-                f"Remove all intermediate basecalled directories {odir}/*, rerun base calling.."
-            )
-            System(f"rm -rf {odir}/*")
+        print(
+            "Found all intermediate directories required from configuration, skip base calling.."
+        )
+        skip_basecalling = True
     else:
         # remove corrupted intermediate directories if any
         print(
@@ -64,92 +58,75 @@ def base_calling_guppy(prefix: str, idir: str, odir: str, barcodes: list, run_th
     print(f"time_elapsed {round(time.time() - ts, 2)} seconds")
     return
 
-def base_calling_dorado(prefix: str, idir: str, odir: str, barcodes: list, sample_sheet: str, run_through=True):
+def base_calling_dorado(prefix: str, idir: str, odir: str, tmpdir: str, barcodes: list, sample_sheet: str):
     print(f">>>Dorado Base calling..")
     ts = time.time()
 
     skip_basecalling = False
     if all(os.path.isdir(f"{odir}/{barcode}") for barcode in barcodes):
-        if run_through:
-            print(
-                "Found all intermediate directories required from configuration, skip base calling.."
-            )
-            skip_basecalling = True
-        else:
-            print(
-                f"Remove all intermediate basecalled directories {odir}/*, rerun base calling.."
-            )
-            for path in os.listdir(odir):
-                if path != "calls.bam":
-                    System(f"rm -rf {odir}/{path}")
+        print(
+            "Found all intermediate directories required from configuration, skip base calling.."
+        )
+        skip_basecalling = True
     else:
         # remove corrupted intermediate directories if any
         print(
                 f"Remove all intermediate basecalled directories {odir}/*, rerun base calling.."
             )
-        for path in os.listdir(odir):
-            if path != "calls.bam":
-                System(f"rm -rf {odir}/{path}")
+        System(f"rm -rf {odir}/*")
 
-    if skip_basecalling:
-        print("Skip base calling..")
-        print(f"time_elapsed {round(time.time() - ts, 2)} seconds")
-        return
+    if not skip_basecalling:
+        # base calling
+        cbamfile = f"{tmpdir}/calls.bam"
+        elogfile = f"{prefix}.basecaller.err.log"
+        ibamfile = ""
+        if os.path.exists(cbamfile):
+            ibamfile = f"{tmpdir}/incomplete.bam"
+            System(f"mv {cbamfile} {ibamfile}")
+        
+        with open(cbamfile, "w") as out, open(elogfile, "w") as err:
+            command = [
+                dorado_basecaller,
+                "basecaller",
+                dorado_model,
+                idir,
+                "--kit-name",
+                dorado_kit
+            ]
+            if ibamfile != "":
+                command += ["--resume-from", ibamfile]
 
-    # base calling
-    cbamfile = f"{odir}/calls.bam"
-    elogfile = f"{prefix}.basecaller.err.log"
-    ibamfile = ""
-    if os.path.exists(cbamfile):
-        ibamfile = f"{odir}/incomplete.bam"
-        System(f"mv {cbamfile} {ibamfile}")
-    
-    with open(cbamfile, "w") as out, open(elogfile, "w") as err:
-        command = [
-            dorado_basecaller,
-            "basecaller",
-            dorado_model,
-            idir,
-            "--kit-name",
-            dorado_kit
-        ]
-        if ibamfile != "":
-            command += ["--resume-from", ibamfile]
+            return_code = Run_Program(command, out, err)
+            out.close(), err.close()
+        
+        if not is_success(return_code, None, elogfile, "dorado base calling"):
+            print("Command: " + " ".join(command))
+            print("Exit..")
+            sys.exit(1)
 
-        return_code = Run_Program(command, out, err)
-        out.close(), err.close()
-    
-    if ibamfile != "":
-        System(f"rm {ibamfile}")
-    
-    if not is_success(return_code, None, elogfile, "dorado base calling"):
-        print("Command: " + " ".join(command))
-        print("Exit..")
-        sys.exit(1)
-
-    # run demux
-    ologfile = f"{prefix}.demux.out.log"
-    elogfile = f"{prefix}.demux.err.log"
-    with open(ologfile, "w") as out, open(elogfile, "w") as err:
-        command = [
-            dorado_basecaller,
-            "demux",
-            "--emit-fastq",
-            "--kit-name",
-            dorado_kit,
-            "--sample-sheet",
-            sample_sheet,
-            "--output-dir",
-            odir,
-            cbamfile
-        ]
-        return_code = Run_Program(command, out, err)
-        out.close(), err.close()
-    
-    if not is_success(return_code, None, elogfile, "dorado demux"):
-        print("Command: " + " ".join(command))
-        print("Exit..")
-        sys.exit(1)
+        # run demux
+        ologfile = f"{prefix}.demux.out.log"
+        elogfile = f"{prefix}.demux.err.log"
+        with open(ologfile, "w") as out, open(elogfile, "w") as err:
+            command = [
+                dorado_basecaller,
+                "demux",
+                "--emit-fastq",
+                "--kit-name",
+                dorado_kit,
+                "--sample-sheet",
+                sample_sheet,
+                "--output-dir",
+                odir,
+                cbamfile
+            ]
+            return_code = Run_Program(command, out, err)
+            out.close(), err.close()
+        
+        if not is_success(return_code, None, elogfile, "dorado demux"):
+            print("Command: " + " ".join(command))
+            print("Exit..")
+            sys.exit(1)
 
     # move fastq to directories
     for barcode in barcodes:
